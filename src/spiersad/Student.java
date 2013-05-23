81c53b45d5ac425e3cac6f03df60633ac295ff62
Adrian <spiersad@gmail.com>
2013-05-22 22:36:01 -0400

    Final

--- src/spiersad/Student.java
+++ src/spiersad/Student.java
@@ -1,51 +1,1 @@
-package spiersad;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created with IntelliJ IDEA.
- * User: spiers
- * Date: 5/21/13
- * Time: 6:17 PM
- * To change this template use File | Settings | File Templates.
- */
-public class Student {
-    private String name;
-    private int ID;
-    private List<Integer> scores = new ArrayList<Integer>();
-    private double average;
-
-    Student(String studentName, int studentID){
-        name =  studentName;
-        ID = studentID;
-    }
-
-    public void getAverage(){
-        average = 0;
-        for (int i = 0; i < scores.size(); i++) {
-            average += scores.get(i);
-        }
-        average /= scores.size();
-    }
-
-    public String toString(){
-        this.getAverage();
-        return "Name: " + name + " ID: " + ID + " Average: " + average;
-    }
-
-    public boolean equals(Student rightSide){
-        return ID == rightSide.ID;
-    }
-
-    public int compareTo(Student rightSide){
-        getAverage();
-        rightSide.getAverage();
-        if (average == rightSide.average)
-            return 0;
-        else if (average < rightSide.average)
-            return -1;
-        else
-            return 1;
-    }
-}
+package spiersad;/** * Created with IntelliJ IDEA. * User: spiers * Date: 5/21/13 * Time: 6:17 PM * To change this template use File | Settings | File Templates. */public class Student {    private final static int ARRAY_SIZE = 5;    private String name;    private int ID;    private int[] scores = new int[ARRAY_SIZE];    private double average;    public String getName() {        return name;    }    public void setName(String name) {        this.name = name;    }    public int getID() {        return ID;    }    public void setID(int ID) {        this.ID = ID;    }    public int getScores(int index) {        return scores[index];    }    public void setScores(int index, int value) {        this.scores[index] = value;    }    public void getAverage(){        average = 0;        for (int i = 0; i < ARRAY_SIZE; i++) {            average += scores[i];        }        average /= ARRAY_SIZE;    }    public String toString(){        this.getAverage();        return "Name: " + name + " ID: " + ID + " Average: " + average;    }    public boolean equals(Student rightSide){        return ID == rightSide.ID;    }    public int compareTo(Student rightSide){        getAverage();        rightSide.getAverage();        if (average == rightSide.average)            return 0;        else if (average < rightSide.average)            return -1;        else            return 1;    }}
